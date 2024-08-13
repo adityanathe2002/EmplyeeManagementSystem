5f4cbb6e99e15da96b8743b6bc922bf33db23d4b
Aditya <adityanathe72@gmail.com>
2024-08-13 20:46:22 +0530

    Initial commit

---
+++ model/Employee.java
@@ -0,0 +1,56 @@
+package com.qsp.model;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private Long contact;
+	private double sal;
+	private String job;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getContact() {
+		return contact;
+	}
+
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+
+	public double getSal() {
+		return sal;
+	}
+
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", contact=" + contact + ", sal=" + sal + ", job=" + job + "]";
+	}
+
+}
